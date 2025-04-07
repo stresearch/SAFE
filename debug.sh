4bc2350856075b7b501d1680ff933cea7bfc42b3
ktrapeznikov <ktrapeznikov@gmail.com>
2025-04-07 10:18:57 -0400

    adding network block (#8)

--- debug.sh
+++ debug.sh
@@ -4,7 +4,10 @@ export SAFE_DATASET_REPO=safe-challenge/safe-challenge-practice-dataset
 # change this to your model that you want to test
 export MODEL_REPO=safe-challenge/safe-example-submission
 export DATASET_PATH=/tmp/data
-export MODEL_PATH=/tmp/model 
+export MODEL_PATH=/tmp/model
+export HF_HUB_ENABLE_HF_TRANSFER=1
+
+set -ex
 
 echo -e "\n\n\n*******************\n\n\n"
 
@@ -22,10 +25,20 @@ ls -R $MODEL_PATH
 
 echo -e "\n\n\n*******************\n\n\n"
 
-cd $MODEL_PATH
+
+echo -e "disabling network"
+# shutil.copyfile("sandbox", f"{submission_dir}/sandbox")
+# sandbox_path = f"{submission_dir}/sandbox"
+# os.chmod(sandbox_path, 0o755)
+# os.chown(sandbox_path, os.getuid(), os.getgid())
+cp /app/sandbox $MODEL_PATH/sandbox
+chmod 755 $MODEL_PATH/sandbox
+chown $UID:$(id -g) $MODEL_PATH/sandbox
+
+cd $MODEL_PATH 
 echo "STEP 4: running script.py"
 cat "script.py"
-python script.py
+sandbox python script.py
 
 echo -e "\n\n\n*******************\n\n\n"
 
